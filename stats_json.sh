3319a5839ce9452a698236becbc67183e1c4eb52
AdamWHY2K <github@adamwhy2k.anonaddy.com>
2025-06-22 15:49:47 +0000

    Create stats_json.sh

---
+++ stats_json.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DEST_DIR="/var/www/json"
+OUTPUT_FILE="${DEST_DIR}/bunkerweb_stats.json"
+
+TOTAL=$(curl -H "Host: bwapi" 127.0.0.1:5000/metrics/errors | jq '.msg | [.[]] | add')
+BLOCKED=$(curl -H "Host: bwapi" 127.0.0.1:5000/metrics/requests | jq '.msg | .requests | length')
+BANS=$(curl -H "Host: bwapi" 127.0.0.1:5000/bans | jq '.data | length')
+COUNTRY=$(curl -H "Host: bwapi" 127.0.0.1:5000/metrics/requests | jq '.msg.requests | map(.country) | group_by(.) | map({country: .[0], count: length}) | max_by(.count) | "\(.country) (\(.count))"')
+
+echo "{\"total\":${TOTAL},\"blocked\":${BLOCKED},\"bans\":${BANS},\"dom_origin\":${COUNTRY}}" > "${OUTPUT_FILE}"
+chown nginx:nginx "${OUTPUT_FILE_BWS}"
