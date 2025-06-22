6a85178409e33dc5560e2eb2287578eb30693fe1
AdamWHY2K <github@adamwhy2k.anonaddy.com>
2025-06-22 18:02:47 +0100

    fix: handle zero blocked country exception
    now displays null (0) in the country section when there have been zero blocked requests

--- stats_json.sh
+++ stats_json.sh
@@ -6,7 +6,7 @@ OUTPUT_FILE="${DEST_DIR}/bunkerweb_stats.json"
 TOTAL=$(curl -H "Host: bwapi" 127.0.0.1:5000/metrics/errors | jq '.msg | [.[]] | add')
 BLOCKED=$(curl -H "Host: bwapi" 127.0.0.1:5000/metrics/requests | jq '.msg | .requests | length')
 BANS=$(curl -H "Host: bwapi" 127.0.0.1:5000/bans | jq '.data | length')
-COUNTRY=$(curl -H "Host: bwapi" 127.0.0.1:5000/metrics/requests | jq '.msg.requests | map(.country) | group_by(.) | map({country: .[0], count: length}) | max_by(.count) | "\(.country) (\(.count))"')
+COUNTRY=$(curl -H "Host: bwapi" 127.0.0.1:5000/metrics/requests | jq '.msg.requests? // [] | map(.country) | group_by(.) | map({country: .[0], count: length}) | max_by(.count) | "\(.country) (\(.count? // 0))"')
 
 echo "{\"total\":${TOTAL},\"blocked\":${BLOCKED},\"bans\":${BANS},\"dom_origin\":${COUNTRY}}" > "${OUTPUT_FILE}"
 chown nginx:nginx "${OUTPUT_FILE_BWS}"
