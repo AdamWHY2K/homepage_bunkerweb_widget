c48bd406a6c06e133a730cb188bfc464e7dc336b
AdamWHY2K <github@adamwhy2k.anonaddy.com>
2025-06-22 19:50:54 +0100

    feat: add country flag emoji

--- stats_json.sh
+++ stats_json.sh
@@ -6,7 +6,8 @@ OUTPUT_FILE="${DEST_DIR}/bunkerweb_stats.json"
 TOTAL=$(curl -H "Host: bwapi" 127.0.0.1:5000/metrics/errors | jq '.msg | [.[]] | add')
 BLOCKED=$(curl -H "Host: bwapi" 127.0.0.1:5000/metrics/requests | jq '.msg | .requests | length')
 BANS=$(curl -H "Host: bwapi" 127.0.0.1:5000/bans | jq '.data | length')
-COUNTRY=$(curl -H "Host: bwapi" 127.0.0.1:5000/metrics/requests | jq '.msg.requests? // [] | map(.country) | group_by(.) | map({country: .[0], count: length}) | max_by(.count) | "\(.country) (\(.count? // 0))"')
+COUNTRY=$(curl -H "Host: bwapi" 127.0.0.1:5000/metrics/requests | jq -r '.msg.requests? // [] | map(.country) | group_by(.) | map({country: .[0], count: length}) | max_by(.count) | "\(.country) (\(.count? // 0))"')
+FLAG=$(echo -e $(printf '\\U%X\\U%X' $(($(printf '%d' "'${COUNTRY:0:1}") + 127397)) $(($(printf '%d' "'${COUNTRY:1}") + 127397))))
 
-echo "{\"total\":${TOTAL},\"blocked\":${BLOCKED},\"bans\":${BANS},\"dom_origin\":${COUNTRY}}" > "${OUTPUT_FILE}"
+echo "{\"total\":${TOTAL},\"blocked\":${BLOCKED},\"bans\":${BANS},\"dom_origin\":\"${FLAG} ${COUNTRY}\"}" > "${OUTPUT_FILE_BWS}"
 chown nginx:nginx "${OUTPUT_FILE_BWS}"
