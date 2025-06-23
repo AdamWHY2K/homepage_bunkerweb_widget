14673e66f77f8a391962d34abdee78e5b5865e54
AdamWHY2K <github@adamwhy2k.anonaddy.com>
2025-06-23 01:03:31 +0100

    fix: remove depricated postfix

--- stats_json.sh
+++ stats_json.sh
@@ -9,5 +9,5 @@ BANS=$(curl -H "Host: bwapi" 127.0.0.1:5000/bans | jq '.data | length')
 COUNTRY=$(curl -H "Host: bwapi" 127.0.0.1:5000/metrics/requests | jq -r '.msg.requests? // [] | map(.country) | group_by(.) | map({country: .[0], count: length}) | max_by(.count) | "\(.country) (\(.count? // 0))"')
 FLAG=$(echo -e $(printf '\\U%X\\U%X' $(($(printf '%d' "'${COUNTRY:0:1}") + 127397)) $(($(printf '%d' "'${COUNTRY:1}") + 127397))))
 
-echo "{\"total\":${TOTAL},\"blocked\":${BLOCKED},\"bans\":${BANS},\"dom_origin\":\"${FLAG} ${COUNTRY}\"}" > "${OUTPUT_FILE_BWS}"
-chown nginx:nginx "${OUTPUT_FILE_BWS}"
+echo "{\"total\":${TOTAL},\"blocked\":${BLOCKED},\"bans\":${BANS},\"dom_origin\":\"${FLAG} ${COUNTRY}\"}" > "${OUTPUT_FILE}"
+chown nginx:nginx "${OUTPUT_FILE}"
